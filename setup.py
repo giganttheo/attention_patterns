24a93a8d988feca4a92ea8814ad54b85da5be753
gigant <giganttheo@gmail.com>
2023-08-14 17:42:49 +0200

    refactoring for package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='attnptrns',
+      version='1.0',
+      description='Attention patterns',
+      author='Théo Gigant',
+      author_email='theo.gigant@l2s.centralesupelec.fr',
+      url='https://github.com/giganttheo/attention_patterns',
+      packages=['attnptrns'],
+     )
