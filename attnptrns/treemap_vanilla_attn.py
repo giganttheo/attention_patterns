fix attention mask input | test

attnptrns/treemap_vanilla_attn.py
@@ -58,11 +58,12 @@ class AttentionPattern():
     m_h = []
     for receivers in receivers_heads:
       h.append(pad_to(receivers, max_graph_len))
+      m_h.append(get_mask(receivers, max_graph_len, attention_mask[0, receivers])) #test
     r = h
     h = []
     for senders in senders_heads:
       h.append(pad_to(senders, max_graph_len))
-      m_h.append(get_mask(senders, max_graph_len, attention_mask[0, senders]))
+      # m_h.append(get_mask(senders, max_graph_len, attention_mask[0, senders])) #test
     m = m_h
     s = h
     return jnp.array(r), jnp.array(s), jnp.array(m)